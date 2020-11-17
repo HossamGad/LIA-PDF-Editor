8fbf2426ec48999b6933d16373b52ef35e7e8bc2
Tero Viitala <vite19ax@student.ju.se>
2020-11-17 19:03:55 +0100

    Enables marking of arrows and text

--- pdf-editor/src/App.js
+++ pdf-editor/src/App.js
@@ -1,22 +1,25 @@
 import React, { Component } from 'react';
 import {Stage, Layer} from 'react-konva';
 import './App.css';
-import { Arrows } from './components/Arrows';
-import { Texts } from './components/Texts';
 import { PDFViewer } from './components/PDFViewer';
 import { Rectangle } from './components/Rectangle';
+import { Arrows } from './components/Arrows';
+import { TextEdit } from './components/TextEdit';
 
-let arrows = [
- 
-];
-
-let texts = [
- 
-];
 
 let idnum = 0;
+let idnum2 = 0;
+let idnum3 = 0;
 
 const initialRectangles = [
+  
+];
+
+const initialArrows = [
+  
+];
+
+const initialTexts = [
   
 ];
 
@@ -24,30 +27,19 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      arrows: arrows,
-      texts: texts,
-      txt: 'Drag this text',
+      
       rectangles: initialRectangles,
-      selectedId: null
+      selectedId: null,
+
+      arrows: initialArrows,
+      selectedId2: null,
+
+      texts: initialTexts,
+      selectedId3: null,
+
     }
     this.canvasRef = React.createRef();
     this.checkDeselect = this.checkDeselect.bind(this);
-  }
-
-  addArrow() {
-    let arrows = this.state.arrows;
-    arrows.push({x:300, y:200});
-    this.setState({
-    arrows: arrows
-    });
-  }
-
-  addText() {
-    let texts = this.state.texts;
-    texts.push({x:400, y:300});
-    this.setState({
-      texts: texts
-    });
   }
 
   addRectangle() {
@@ -69,12 +61,62 @@ class App extends Component {
     });
   }
 
+  addArrow() {
+
+    idnum2++;
+
+    let arrows = this.state.arrows;
+        arrows.push({
+        x: 90,
+        y: 90,
+        points: [0, 0, 100, 100],
+        pointerLength: 20,
+        pointerWidth: 20,
+        fill: 'Red',
+        stroke: 'Red',
+        strokeWidth: 5,
+        id: 'arrow' + idnum2
+    });
+    this.setState({
+      arrows: arrows
+    });
+  }
+
+  addText() {
+
+    idnum3++;
+
+    let textarea = document.getElementById('text-area');
+
+    let txt3 = textarea.value;
+
+    if(txt3 === '') {
+      alert('You have to write something in the input field.');
+      return;
+    }
+    
+    let texts = this.state.texts;
+      texts.push({
+      x: 110,
+      y: 210,
+      text: txt3,
+      fontSize: 50,
+      fontFamily: 'Calibri',
+      id: 'text' + idnum3
+    });
+    this.setState({
+      texts: texts
+    });
+  }
+
   checkDeselect(e) {
     // deselect when clicked on empty area
     const clickedOnEmpty = e.target === e.target.getStage();
     if (clickedOnEmpty) {
         this.setState({
-          selectedId: null
+          selectedId: null,
+          selectedId2: null,
+          selectedId3: null
         });
     }
   };
@@ -89,6 +131,7 @@ class App extends Component {
             <button className="btn" style={{float:"right", marginRight:40}} onClick={() => this.addRectangle()}><i className="far fa-square"></i></button>
             <button className="btn" style={{float:"right", marginRight:5}} onClick={() => this.addArrow()}><i className="fas fa-location-arrow"></i></button>
             <button className="btn" style={{float:"right", marginRight:5}} onClick={() => this.addText()}><i className="fas fas fa-font"></i></button>
+            <input id="text-area" placeholder=" Write text here..."style={{float:"right", marginRight:5, margin:5, height: 35, fontSize: 20}} />
         </div>
 
         <Stage id="id-stage" width={1224} height={1200}
@@ -96,8 +139,6 @@ class App extends Component {
           onTouchStart={this.checkDeselect}
         >
           <Layer>
-            <Arrows arrows={arrows} />
-            <Texts texts={texts} />
             
             {this.state.rectangles.map((rect, i) => {
               return (
@@ -115,6 +156,50 @@ class App extends Component {
                     rects[i] = newAttrs;
                     this.setState({
                       rectangles: rects
+                    })
+                  }}
+                />
+              );
+            })}
+
+            {this.state.arrows.map((arrow, i) => {
+              return (
+                <Arrows
+                  key={i}
+                  shapeProps={arrow}
+                  isSelected={arrow.id === this.state.selectedId2}
+                  onSelect={() => {
+                    this.setState({
+                      selectedId2: arrow.id
+                    })
+                  }}
+                  onChange={(newAttrs) => {
+                    const arrows1 = this.state.arrows.slice();
+                    arrows1[i] = newAttrs;
+                    this.setState({
+                      arrows: arrows1
+                    })
+                  }}
+                />
+              );
+            })}
+
+            {this.state.texts.map((text, i) => {
+              return (
+                <TextEdit
+                  key={i}
+                  shapeProps={text}
+                  isSelected={text.id === this.state.selectedId3}
+                  onSelect={() => {
+                    this.setState({
+                      selectedId3: text.id
+                    })
+                  }}
+                  onChange={(newAttrs) => {
+                    const texts = this.state.texts.slice();
+                    texts[i] = newAttrs;
+                    this.setState({
+                      texts: texts
                     })
                   }}
                 />
